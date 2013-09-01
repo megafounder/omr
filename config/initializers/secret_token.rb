fc9a29bf412f078ada7f7fe61fc6cc2496753683
Jonathan Garcia <ceo@megafounder.com>
2013-09-01 17:14:08 +0200

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Omr::Application.config.secret_key_base = 'd6ed3bbef1ecab6c8f19d8ed98debb9037f8d7249c1cccce9d462b1a6c630b94a8efb924cbfb3a33cd8202ee2e8d8ffabcbee24c4b2e505a537f89d6b49a3ab2'
